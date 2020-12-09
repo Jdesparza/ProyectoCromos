b0798fa922902e7d0a2bd51efac6fcf785c91878
Jeferson <49170865+jecueva1997@users.noreply.github.com>
2020-12-09 09:42:56 -0500

    Add files via upload

---
+++ ProyectoIntegrador/dll/config.php
@@ -0,0 +1,9 @@
+<?php 
+    $site_url="http://127.0.0.1/ProyectoIntegrador/";
+    $local_path="C:\\xampp\htdocs\ProyectoIntegrador";
+	//datos de la base de datos
+	define('DBHOST', '127.0.0.1');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
+	define('DBNAME', 'personas');
+?>
