c9e066a43cd5e1271546a19e90faea72d5d78484
andresvallejoz1991 <andresvallejoz@hotmail.com>
2021-01-11 11:45:50 -0500

    Update

--- Proyect/resources/views/juego.blade.php
+++ Proyect/resources/views/juego.blade.php
@@ -8,29 +8,29 @@
     <section class ="articulos">
         <article class ="imagen">
             <a href="macro"><img src="../../img/macroeconomia 2 (1).png" alt=""></a>
-        <h5>Macroeconomia</h5>
+        <h5>Macroeconomía</h5>
         </article>
         <article class="imagen">
         <a href="micro"><img src="../../img/Microeconomia.png" alt=""></a>
-        <h5>Microeconomia</h5>
+        <h5>Microeconomía</h5>
         </article>
         <article class="imagen">
         <a href="econ"><img src="../../img/que-es-la-econometria-800x452 1.png" alt=""></a>
-        <h5>Econometria</h5>
+        <h5>Econometría</h5>
         </article>
     </section>
     <section class ="articulos">
         <article class ="imagen">
             <img src="../../img/macroeconomia 2 (1).png" alt="">
-        <h5>Tema 4</h5>
+        <h5>Macroeconomía</h5>
         </article>
         <article class="imagen">
         <img src="../../img/Microeconomia.png" alt="">
-        <h5>Tema 5</h5>
+        <h5>Microeconomía</h5>
         </article>
         <article class="imagen">
         <img src="../../img/que-es-la-econometria-800x452 1.png" alt="">
-        <h5>Tema 6</h5>
+        <h5>Econometría</h5>
         </article>
     </section>
 </div>
